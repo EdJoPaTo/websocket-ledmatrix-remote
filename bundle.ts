fbee1a295577f897b41819c41b7d5570cbb51afc
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-04-30 00:04:07 +0200

    build(deno): update dependencies

--- bundle.ts
+++ bundle.ts
@@ -1,3 +1,3 @@
-import { bundle } from "https://deno.land/x/emit@0.19.0/mod.ts";
+import { bundle } from "https://deno.land/x/emit@0.21.1/mod.ts";
 const { code } = await bundle("client-web.ts");
 await Deno.writeTextFile("public/client.js", code);
