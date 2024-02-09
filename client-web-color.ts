8835e245bb81a65832660382b8d3145893020409
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-09 13:43:07 +0100

    feat: use HTML5 color picker

--- client-web-color.ts
+++ client-web-color.ts
@@ -1,24 +1,30 @@
-const rInput = document.querySelector("#r") as HTMLInputElement;
-const gInput = document.querySelector("#g") as HTMLInputElement;
-const bInput = document.querySelector("#b") as HTMLInputElement;
+const colorInput = document.querySelector("#color") as HTMLInputElement;
+colorInput.value = generateDefaultColor();
+colorInput.addEventListener("change", updateColor);
 
-rInput.value = Math.floor(Math.random() * 256).toFixed(0);
-gInput.value = Math.floor(Math.random() * 256).toFixed(0);
-bInput.value = Math.floor(Math.random() * 256).toFixed(0);
-
-rInput.addEventListener("change", updateTextcolor);
-gInput.addEventListener("change", updateTextcolor);
-bInput.addEventListener("change", updateTextcolor);
-updateTextcolor();
-function updateTextcolor() {
-	const { r, g, b } = getCurrentColor();
-	document.documentElement.style.setProperty("--color", `rgb(${r},${g},${b})`);
-}
-
+let currentColor: { r: number; g: number; b: number } = { r: 0, g: 0, b: 0 };
 export function getCurrentColor(): { r: number; g: number; b: number } {
-	return {
-		r: Number(rInput.value),
-		g: Number(gInput.value),
-		b: Number(bInput.value),
-	};
+	return currentColor;
 }
+
+updateColor();
+function updateColor() {
+	document.documentElement.style.setProperty("--color", colorInput.value);
+
+	const h = colorInput.value;
+	const r = Number("0x" + h[1] + h[2]);
+	const g = Number("0x" + h[3] + h[4]);
+	const b = Number("0x" + h[5] + h[6]);
+	currentColor = { r, g, b };
+}
+
+function generateDefaultColor() {
+	let r = Math.floor(Math.random() * 256).toString(16);
+	let g = Math.floor(Math.random() * 256).toString(16);
+	let b = Math.floor(Math.random() * 256).toString(16);
+	if (r.length === 1) r = "0" + r;
+	if (g.length === 1) g = "0" + g;
+	if (b.length === 1) b = "0" + b;
+	return "#" + r + g + b;
+}
+console.log(generateDefaultColor());
